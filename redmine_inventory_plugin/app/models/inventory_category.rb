cadbcf4e6e4ce75fb590519248b47a844cd15fa4
Bryt Li <bryt@Bryts-MacBook-Pro.local>
2017-09-19 23:17:00 +0800

    remove inventory

--- redmine_inventory_plugin/app/models/inventory_category.rb
+++
@@ -1,9 +0,0 @@
-class InventoryCategory < ActiveRecord::Base
-  #t.column :name, :string
-  #t.column :description, :text
-  
-  has_many :inventory_parts
-  
-  validates_presence_of :name
-
-end
--- redmine_inventory_plugin/db/migrate/008_modifications_for_permission.rb
+++
@@ -1,9 +0,0 @@
-class ModificationsForPermission < ActiveRecord::Migration
-    def self.up
-      add_column :inventory_warehouses, :user_manager_id, :integer
-    end
-
-    def self.down
-      remove_column :inventory_warehouses, :user_manager_id
-    end
-  end
